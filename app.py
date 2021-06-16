2633c283efec2fc41dc15aeab838ebfe08f38288
Aric <hernandoaric@gmail.com>
2021-06-16 17:44:43 +0700

    created app factory

--- app.py
+++ app.py
@@ -7,25 +7,28 @@ from engine import get_engine_move
 import chess
 import json
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+def get_app():
+    app = Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/naive', methods=['POST'])
-def naive():
-    req = request.get_json()
-    fen = req["fen"]
-    color = fen.split(" ")[1]
-    if(color == "w"):
-        color = "white"
-    elif(color == "b"):
-        color = "black"
-    response = {}
-    response['move'] = getMoveMinimaxStr(fen, color)
-    return jsonify(response)
+    @app.route('/naive', methods=['POST'])
+    def naive():
+        req = request.get_json()
+        fen = req["fen"]
+        color = fen.split(" ")[1]
+        if(color == "w"):
+            color = "white"
+        elif(color == "b"):
+            color = "black"
+        response = {}
+        response['move'] = getMoveMinimaxStr(fen, color)
+        return jsonify(response)
 
-@app.route('/engine', methods=['POST'])
-def engine():
-    req = request.get_json()
-    res = {'move' : str(get_engine_move(req["fen"]))}
-    return jsonify(res)
+    @app.route('/engine', methods=['POST'])
+    def engine():
+        req = request.get_json()
+        res = {'move' : str(get_engine_move(req["fen"]))}
+        return jsonify(res)
+
+    return app
