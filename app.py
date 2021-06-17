909009235207d2728b1d014df6a333e5d10b4a37
Aric <hernandoaric@gmail.com>
2021-06-17 16:45:13 +0700

    debugging

--- app.py
+++ app.py
@@ -8,40 +8,41 @@ import chess
 import json
 
 app = Flask(__name__)
-app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/naive', methods=['POST'])
-def naive():
-    req = request.get_json()
-    fen = req["fen"]
-    color = fen.split(" ")[1]
-    if(color == "w"):
-        color = "white"
-    elif(color == "b"):
-        color = "black"
-    response = jsonify({'move' : getMoveMinimaxStr(fen, color)})
-    print(response)
-    # response.headers.add('Access-Control-Allow-Origin', 'https://chess-webapp.com/')
-    return response
+# @app.route('/naive', methods=['POST'])
+# def naive():
+#     req = request.get_json()
+#     fen = req["fen"]
+#     color = fen.split(" ")[1]
+#     if(color == "w"):
+#         color = "white"
+#     elif(color == "b"):
+#         color = "black"
+#     response = jsonify({'move' : getMoveMinimaxStr(fen, color)})
+#     print(response)
+#     # response.headers.add('Access-Control-Allow-Origin', 'https://chess-webapp.com/')
+#     return response
 
-@app.route('/engine', methods=['POST'])
-def engine():
-    req = request.get_json()
-    res = {'move' : str(get_engine_move(req["fen"]))}
-    response = jsonify(res)
-    # response.headers.add('Access-Control-Allow-Origin', 'https://chess-webapp.com/')
+# @app.route('/engine', methods=['POST'])
+# def engine():
+#     req = request.get_json()
+#     res = {'move' : str(get_engine_move(req["fen"]))}
+#     response = jsonify(res)
+#     # response.headers.add('Access-Control-Allow-Origin', 'https://chess-webapp.com/')
     
-    return response
+#     return response
 
-@app.route('/testmove', methods=['GET','POST'])
-def testmove():
-    return jsonify({'move' : "d2d4"})
-@app.route('/test', methods=['GET'])
-def test():
-    return "it does somewhat work"
+# @app.route('/testmove', methods=['GET','POST'])
+# def testmove():
+#     return jsonify({'move' : "d2d4"})
+# @app.route('/test', methods=['GET'])
+# def test():
+#     return "it does somewhat work"
+
 @app.route('/', methods=['GET'])
-def base():
+def index():
     return "it does somewhat work"
 
 @app.errorhandler(404)
